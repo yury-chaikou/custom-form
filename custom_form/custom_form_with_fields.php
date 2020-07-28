735bdcf6ce5743cfaaddf08c136e5c63bacf7a66
yury-chaikou <68887857+yury-chaikou@users.noreply.github.com>
2020-07-28 14:50:05 +0300

    Update custom_form_with_fields.php
    Remove feature create contact at hubspot.com for users 
    Add feature create contact at hubspot.com for users with API

--- custom_form/custom_form_with_fields.php
+++ custom_form/custom_form_with_fields.php
@@ -18,11 +18,6 @@ wp_register_style( 'custom_form_with_fields',
 	'/wp-content/plugins/custom_form_with_fields/assets/css/style.css' );
 wp_enqueue_style( 'custom_form_with_fields' );
 
-function custom_form_with_fields_scripts() {
-	wp_enqueue_script( 'hubspot', '//js.hs-scripts.com/7805462.js',  '' , '', true );
-}
-add_action( 'admin_enqueue_scripts', 'custom_form_with_fields_scripts' );
-
 function custom_form_with_fields_options_page() {
 
 	echo '<h1>Custom form with fields options page<h1>';
@@ -71,23 +66,65 @@ function custom_form_with_fields_options_page() {
 	} else {
 		$subject           = htmlspecialchars( $_POST['subject'] );
 		$message           = htmlspecialchars( $_POST['message'] );
+		$firstname         = htmlspecialchars( $_POST['first_name'] );
+		$lastname          = htmlspecialchars( $_POST['last_name'] );
+		$email             = htmlspecialchars( $_POST['email'] );
 		$formatted_message = nl2br( $message );
+		$firstname         = urldecode( $firstname );
+		$lastname          = urldecode( $lastname );
+		$email             = urldecode( $email );
 		$subject           = urldecode( $subject );
 		$message           = urldecode( $message );
 		$message           = trim( $message );
 
-		if ( wp_mail( "yury80@gmail.com", 'subject: '. "$subject", 'message: '. "$message" ) ) {
+		$arr = [
+			'properties' => [
+				[
+					'property' => 'email',
+					'value'    => $email,
+				],
+				[
+					'property' => 'firstname',
+					'value'    => $firstname,
+				],
+				[
+					'property' => 'lastname',
+					'value'    => $lastname,
+				],
+			],
+		];
+
+		$post_json = json_encode( $arr );
+		$hapikey   = 'f23b2a6e-ab7b-495b-af36-e043f5b3897b';
+		$endpoint  = 'https://api.hubapi.com/contacts/v1/contact?hapikey=' . $hapikey;
+		$ch        = @curl_init();
+		@curl_setopt( $ch, CURLOPT_POST, true );
+		@curl_setopt( $ch, CURLOPT_POSTFIELDS, $post_json );
+		@curl_setopt( $ch, CURLOPT_URL, $endpoint );
+		@curl_setopt( $ch, CURLOPT_HTTPHEADER, [ 'Content-Type: application/json' ] );
+		@curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
+		$response    = @curl_exec( $ch );
+		$status_code = @curl_getinfo( $ch, CURLINFO_HTTP_CODE );
+		$curl_errors = curl_error( $ch );
+		@curl_close( $ch );
+		$Api_log = new Log_file( __DIR__ . '/logs/apilog.log' );
+		$api_log =
+			"curl Errors: " . $curl_errors . "\nStatus code: " . $status_code . "\nResponse: " .
+			$response;
+		$Api_log->write_to_file( $api_log );
+
+
+		if ( wp_mail( "yury80@gmail.com", 'subject: ' . "$subject", 'message: ' . "$message" ) ) {
 			echo "<p>Message sent <br> Your message is: " . " $formatted_message" . "</p>";
 			$Log = new Log_file( __DIR__ . '/logs/log.log' );
 			$log = 'author:  ' . $_POST['email'] . "\n" . 'at ' . date( 'Y-m-d H:i:s' ) . "\n" .
 			       ' wrote: ' . "$message" . "\n" . "\n";
 			$Log->write_to_file( $log );
+
 		} else {
 			echo '<p>Message not sent</p>';
 		}
 	}
-
 }
+
 add_action( 'admin_menu', 'custom_form_with_fields_add_admin_pages' );
-
-
